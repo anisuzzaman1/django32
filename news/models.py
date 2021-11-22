5c73e0fdcc42bf0cf1df8f97a199e0eb8816e9ed
anisuzzaman1 <anisuzzaman2112@gmail.com>
2021-11-22 21:44:16 +0600

    Try to change DB but failed

--- news/models.py
+++ news/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 
 class News(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=250)
     content =  models.TextField()
+    #status = models.CharField(max_length=250)
